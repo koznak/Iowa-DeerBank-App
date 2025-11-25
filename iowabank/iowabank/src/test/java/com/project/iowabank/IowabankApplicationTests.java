3d4975964f3e3108dd138ca99f2c19481bb0f6ce
iTanveerAhmad <talktanveerkhan@outlook.com>
2025-11-25 14:38:56 -0600

    Initial commit

---
+++ iowabank/iowabank/src/test/java/com/project/iowabank/IowabankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.iowabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IowabankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
